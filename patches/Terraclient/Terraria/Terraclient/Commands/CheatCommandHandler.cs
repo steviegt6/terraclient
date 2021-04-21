fa2e5f2fd28da6e758cc32366a27d5df421e35aa
Stevie <xxlennygamerxx@gmail.com>
2021-04-20 18:00:04 -0700

    uuid changing

---
+++ patches/Terraclient/Terraria/Terraclient/Commands/CheatCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Terraria.Terraclient.Commands
+{
+	public static class CheatCommandHandler
+	{
+		public static bool ParseCheatCommand(string message) {
+			if (!message.StartsWith("."))
+				return false;
+
+			return true;
+		}
+	}
+}
