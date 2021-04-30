dec9db38513ca339bf0a0c1b9c66f94bda74dbf2
Stevie <xxlennygamerxx@gmail.com>
2021-04-29 23:33:54 -0700

    cleanup and methodbuilders

--- patches/Terraclient/Terraria/Terraclient/Commands/MystagogueCommand.cs
+++ patches/Terraclient/Terraria/Terraclient/Commands/MystagogueCommand.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -9,289 +8,232 @@ namespace Terraria.Terraclient.Commands
 {
 	public class MystagogueCommand
 	{
-		public MystagogueCommand(string commandName, string commandDescription, Action<List<string>> commandAction) {
-			this.commandName = commandName;
-			this.commandDescription = commandDescription;
-			this.commandAction = commandAction;
-			MystagogueCommand.commandList.Add(this);
+		private MystagogueCommand(string commandName, string commandDescription) {
+			CommandName = commandName;
+			CommandDescription = commandDescription;
 		}
 
+		public static MystagogueCommand Create(string commandName, string commandDescription) =>
+			new(commandName, commandDescription);
+
+		public MystagogueCommand AddAction(Action<List<string>> action) {
+			CommandActions.Add(action);
+			return this;
+		}
+
+		public MystagogueCommand Build() {
+			CommandList.Add(this);
+			return this;
+		}
+
+		// TODO: localization
 		static MystagogueCommand() {
-			new MystagogueCommand("help", "Returns \"test\" in chat.", new Action<List<string>>((List<string> args) => {
-				CheatCommandUtils.Output(false, "test");
-			}));
-			new MystagogueCommand("i", "(Name-Concatenated/ID, Stack, Prefix) Spawns an item by converting your currently held cursor item (or thin air) to it.", new Action<List<string>>((List<string> args) => {
-				if (args.Count == 0) {
-					CheatCommandUtils.Output(true, "That command requires arguments");
-					return;
-				}
-				int finalItemSelection = 0;
-				if (!new Regex("\\D").IsMatch(args[0])) {
-					string text = args[0];
-					while (text.StartsWith("0")) {
-						text = text.Remove(0, 1);
-					}
-					while (text != finalItemSelection.ToString()) {
-						if (finalItemSelection == ItemID.Count) {
-							CheatCommandUtils.Output(true, "Given item ID does not correspond to an item");
-							return;
-						}
-						finalItemSelection++;
-					}
-					args.RemoveAt(0);
-				}
-				else {
-					int indexOfStackCountArg = 1;
-					if (args.Count < 0) {
-						while (new Regex("\\D").IsMatch(args[indexOfStackCountArg])) {
-							indexOfStackCountArg++;
-							if (indexOfStackCountArg == args.Count) {
-								break;
-							}
-						}
-					}
-					string fullItemNameQuery = string.Join(" ", args.GetRange(0, indexOfStackCountArg)).ToLower();
-					args = args.GetRange(indexOfStackCountArg, args.Count - indexOfStackCountArg);
-					List<int> foundItems = new List<int>();
-					for (int i = 0; i < ItemID.Count; i++) {
-						if (Lang.GetItemNameValue(i).ToLower().StartsWith(fullItemNameQuery)) {
-							foundItems.Add(i);
-						}
-					}
-					if (foundItems.Count == 0) {
-						CheatCommandUtils.Output(true, "No item names match");
+			Create("help", "Returns \"test\" in chat")
+				.AddAction(_ => CheatCommandUtils.Output(false, "test"))
+				.Build();
+
+			Create("i",
+					"(name-concatenated/ID, stack, prefix) Spawns an item by converting your currently-held cursor item (or thin air) to it. " +
+					"Automatically sent to your inventory if your inventory is closed.")
+				.AddAction(args => {
+					if (args.Count == 0) {
+						CheatCommandUtils.Output(true, "That command requires arguments");
 						return;
 					}
-					if (foundItems.Count > 1) {
-						List<string> itemNameListToCheck = new List<string>();
-						foreach (int id in foundItems) {
-							itemNameListToCheck.Add(Lang.GetItemNameValue(id).ToLower());
-						}
-						List<string> itemNameAndIDsToOutput = new List<string>();
-						foreach (int id in foundItems) {
-							itemNameAndIDsToOutput.Add(string.Concat(new object[]
-							{
-								Lang.GetItemNameValue(id),
-								" (",
-								id,
-								")"
-							}));
-						}
-						for (int k = 0; k < itemNameListToCheck.Count; k++) {
-							if (itemNameListToCheck[k] == fullItemNameQuery) {
-								finalItemSelection = foundItems[k];
-								itemNameAndIDsToOutput.RemoveAt(k);
-								CheatCommandUtils.Output(false, "Other matches include " + string.Join(", ", itemNameAndIDsToOutput));
-								break;
-							}
-							if (k + 1 == foundItems.Count) {
-								CheatCommandUtils.Output(true, "Query too unspecific, found " + string.Join(", ", itemNameAndIDsToOutput));
+
+					int itemType = 0;
+					if (!new Regex("\\D").IsMatch(args[0])) {
+						string text = args[0];
+
+						while (text.StartsWith("0"))
+							text = text.Remove(0, 1);
+
+						while (text != itemType.ToString()) {
+							if (itemType == ItemID.Count) {
+								CheatCommandUtils.Output(true, "Given item ID does not correspond to an item");
 								return;
 							}
+
+							itemType++;
 						}
+
+						args.RemoveAt(0);
 					}
 					else {
-						finalItemSelection = foundItems[0];
+						int stackIndex = 1;
+
+						if (args.Count < 0) {
+							while (new Regex("\\D").IsMatch(args[stackIndex])) {
+								stackIndex++;
+
+								if (stackIndex == args.Count)
+									break;
+							}
+						}
+
+						string nameQuery = string.Join(" ", args.GetRange(0, stackIndex)).ToLower();
+						args = args.GetRange(stackIndex, args.Count - stackIndex);
+
+						List<int> foundItems = new List<int>();
+
+						for (int i = 0; i < ItemID.Count; i++)
+							if (Lang.GetItemNameValue(i).ToLower().StartsWith(nameQuery))
+								foundItems.Add(i);
+
+						switch (foundItems.Count) {
+							case 0:
+								CheatCommandUtils.Output(true, "No item names match");
+								return;
+
+							case > 1: {
+								List<string> itemNameListToCheck =
+									foundItems.Select(id => Lang.GetItemNameValue(id).ToLower()).ToList();
+								List<string> itemNameAndIDsToOutput = foundItems
+									.Select(id => string.Concat(Lang.GetItemNameValue(id), " (", id, ")")).ToList();
+
+								for (int k = 0; k < itemNameListToCheck.Count; k++) {
+									if (itemNameListToCheck[k] == nameQuery) {
+										itemType = foundItems[k];
+										itemNameAndIDsToOutput.RemoveAt(k);
+										CheatCommandUtils.Output(false,
+											"Other matches include " + string.Join(", ", itemNameAndIDsToOutput));
+										break;
+									}
+
+									if (k + 1 != foundItems.Count)
+										continue;
+
+									CheatCommandUtils.Output(true,
+										"Query too unspecific, found " + string.Join(", ", itemNameAndIDsToOutput));
+									return;
+								}
+
+								break;
+							}
+
+							default:
+								itemType = foundItems[0];
+								break;
+						}
 					}
-				}
-				int stack = 1;
-				if (args.Count >= 1) {
-					if (new Regex("\\D").IsMatch(args[0])) {
-						CheatCommandUtils.Output(true, "Stack must be a positive integer");
-						return;
-					}
-					string parsingString = args[0];
-					while (parsingString.StartsWith("0")) {
-						parsingString = parsingString.Remove(0, 1);
-					}
-					if (parsingString.Length > 10) {
-						stack = int.MaxValue;
-					}
-					else if (Convert.ToInt64(parsingString) > 2147483647L) {
-						stack = int.MaxValue;
-					}
-					else if (parsingString.Length > 0) {
-						stack = int.Parse(parsingString);
-					}
-				}
-				int finalPrefixSelection = 0;
-				if (args.Count >= 2) {
-					if (!new Regex("\\D").IsMatch(args[1])) {
-						string parsingString = args[1];
-						while (parsingString.StartsWith("0")) {
+
+					int stack = 1;
+					if (args.Count >= 1) {
+						if (new Regex("\\D").IsMatch(args[0])) {
+							CheatCommandUtils.Output(true, "Stack must be a positive integer");
+							return;
+						}
+
+						string parsingString = args[0];
+
+						while (parsingString.StartsWith("0"))
 							parsingString = parsingString.Remove(0, 1);
-						}
-						while (parsingString != finalPrefixSelection.ToString()) {
-							if (finalPrefixSelection == PrefixID.Count) {
-								CheatCommandUtils.Output(true, "Given prefix ID does not correspond to a prefix");
-								return;
-							}
-							finalPrefixSelection++;
-						}
+
+						if (parsingString.Length > 10)
+							stack = int.MaxValue;
+
+						else if (Convert.ToInt64(parsingString) > 2147483647L)
+							stack = int.MaxValue;
+
+						else if (parsingString.Length > 0)
+							stack = int.Parse(parsingString);
 					}
-					else {
-						string parsingString = args[1].ToLower();
-						List<int> foundPrefixes = new List<int>();
-						for (int i = 0; i < MystagogueCommand.Prefixes.Length; i++) {
-							if (MystagogueCommand.Prefixes[i].ToLower().StartsWith(parsingString) && i != 75 && i != 43 && i != 76) {
-								foundPrefixes.Add(i);
+
+					int finalPrefixSelection = 0;
+
+					if (args.Count >= 2) {
+						if (!new Regex("\\D").IsMatch(args[1])) {
+							string parsingString = args[1];
+
+							while (parsingString.StartsWith("0"))
+								parsingString = parsingString.Remove(0, 1);
+
+							while (parsingString != finalPrefixSelection.ToString()) {
+								if (finalPrefixSelection == PrefixID.Count) {
+									CheatCommandUtils.Output(true, "Given prefix ID does not correspond to a prefix");
+									return;
+								}
+
+								finalPrefixSelection++;
 							}
-						}
-						if (foundPrefixes.Count == 0) {
-							CheatCommandUtils.Output(true, "No prefix names match");
-							return;
-						}
-						if (foundPrefixes.Count > 1) {
-							List<string> prefixNamesAndIDsToOutput = new List<string>();
-							foreach (int i in foundPrefixes) {
-								prefixNamesAndIDsToOutput.Add(string.Concat(new object[]
-								{
-									MystagogueCommand.Prefixes[i],
-									" (",
-									i,
-									")"
-								}));
-							}
-							CheatCommandUtils.Output(true, "Prefix query too unspecific, found " + string.Join(", ", prefixNamesAndIDsToOutput));
-							return;
-						}
-						finalPrefixSelection = foundPrefixes[0];
-					}
-					if (finalPrefixSelection == 18 || finalPrefixSelection == 75) {
-						if (!ContentSamples.ItemsByType[finalItemSelection].accessory) {
-							finalPrefixSelection = 18;
 						}
 						else {
-							finalPrefixSelection = 75;
+							string parsingString = args[1].ToLower();
+							List<int> foundPrefixes = new List<int>();
+
+							for (int i = 0; i < Prefixes.Length; i++)
+								if (Prefixes[i].ToLower().StartsWith(parsingString) && i != 75 && i != 43 && i != 76)
+									foundPrefixes.Add(i);
+
+							switch (foundPrefixes.Count) {
+								case 0:
+									CheatCommandUtils.Output(true, "No prefix names match");
+									return;
+
+								case > 1: {
+									List<string> prefixNamesAndIDsToOutput = foundPrefixes
+										.Select(i => string.Concat(Prefixes[i], " (", i, ")")).ToList();
+									CheatCommandUtils.Output(true,
+										"Prefix query too unspecific, found " +
+										string.Join(", ", prefixNamesAndIDsToOutput));
+									return;
+								}
+
+								default:
+									finalPrefixSelection = foundPrefixes[0];
+									break;
+							}
+						}
+
+						if (finalPrefixSelection is 18 or 75) {
+							finalPrefixSelection = !ContentSamples.ItemsByType[itemType].accessory
+								? 18
+								: 75;
+						}
+
+						if (finalPrefixSelection is 20 or 43) {
+							finalPrefixSelection = ContentSamples.ItemsByType[itemType].ranged
+								? 20
+								: 43;
+						}
+
+						if (finalPrefixSelection is 42 or 76) {
+							finalPrefixSelection = !ContentSamples.ItemsByType[itemType].accessory
+								? 42
+								: 76;
 						}
 					}
-					if (finalPrefixSelection == 20 || finalPrefixSelection == 43) {
-						if (ContentSamples.ItemsByType[finalItemSelection].ranged) {
-							finalPrefixSelection = 20;
-						}
-						else {
-							finalPrefixSelection = 43;
-						}
-					}
-					if (finalPrefixSelection == 42 || finalPrefixSelection == 76) {
-						if (!ContentSamples.ItemsByType[finalItemSelection].accessory) {
-							finalPrefixSelection = 42;
-						}
-						else {
-							finalPrefixSelection = 76;
-						}
-					}
-				}
-				Main.mouseItem.SetDefaults(finalItemSelection);
-				Main.mouseItem.stack = stack;
-				Main.mouseItem.prefix = (byte)finalPrefixSelection;
-				Main.mouseItem.Refresh();
-				string text5 = "";
-				if (Main.mouseItem.prefix > 0) {
-					text5 = " " + MystagogueCommand.Prefixes[(int)Main.mouseItem.prefix];
-				}
-				CheatCommandUtils.Output(false,
-					"Set cursor item to " + Main.mouseItem.stack +
-					(Main.mouseItem.prefix > 0 ? (" " + MystagogueCommand.Prefixes[(int)Main.mouseItem.prefix]) : "") + " " +
-					Lang.GetItemNameValue(Main.mouseItem.type) + " (" + Main.mouseItem.type + ")");
-			}));
+
+					Main.mouseItem.SetDefaults(itemType);
+					Main.mouseItem.stack = stack;
+					Main.mouseItem.prefix = (byte)finalPrefixSelection;
+					Main.mouseItem.Refresh();
+
+					CheatCommandUtils.Output(false,
+						$"Set cursor item to {Main.mouseItem.stack}{(Main.mouseItem.prefix > 0 ? (" " + Prefixes[Main.mouseItem.prefix]) : "")} {Lang.GetItemNameValue(Main.mouseItem.type)} ({Main.mouseItem.type})");
+				})
+				.Build();
 		}
 
-		public string commandName;
+		public string CommandName;
 
-		public string commandDescription;
+		public string CommandDescription;
 
-		public Action<List<string>> commandAction;
+		public List<Action<List<string>>> CommandActions;
 
-		public static List<MystagogueCommand> commandList = new List<MystagogueCommand>();
+		public static List<MystagogueCommand> CommandList = new();
 
 
-		private static string[] Prefixes = new string[]
-		{
-			"Basic",
-			"Large",
-			"Massive",
-			"Dangerous",
-			"Savage",
-			"Sharp",
-			"Pointy",
-			"Tiny",
-			"Terrible",
-			"Small",
-			"Dull",
-			"Unhappy",
-			"Bulky",
-			"Shameful",
-			"Heavy",
-			"Light",
-			"Sighted",
-			"Rapid",
-			"Hasty",
-			"Intimidating",
-			"Deadly",
-			"Staunch",
-			"Awful",
-			"Lethargic",
-			"Awkward",
-			"Powerful",
-			"Mystic",
-			"Adept",
-			"Masterful",
-			"Inept",
-			"Ignorant",
-			"Deranged",
-			"Intense",
-			"Taboo",
-			"Celestial",
-			"Furious",
-			"Keen",
-			"Superior",
-			"Forceful",
-			"Broken",
-			"Damaged",
-			"Shoddy",
-			"Quick",
-			"Deadly",
-			"Agile",
-			"Nimble",
-			"Murderous",
-			"Slow",
-			"Sluggish",
-			"Lazy",
-			"Annoying",
-			"Nasty",
-			"Manic",
-			"Hurtful",
-			"Strong",
-			"Unpleasant",
-			"Weak",
-			"Ruthless",
-			"Frenzying",
-			"Godly",
-			"Demonic",
-			"Zealous",
-			"Hard",
-			"Guarding",
-			"Armored",
-			"Warding",
-			"Arcane",
-			"Precise",
-			"Lucky",
-			"Jagged",
-			"Spiked",
-			"Angry",
-			"Menacing",
-			"Brisk",
-			"Fleeting",
-			"Hasty",
-			"Quick",
-			"Wild",
-			"Rash",
-			"Intrepid",
-			"Violent",
-			"Legendary",
-			"Unreal",
-			"Mythical"
+		private static readonly string[] Prefixes = {
+			"Basic", "Large", "Massive", "Dangerous", "Savage", "Sharp", "Pointy", "Tiny", "Terrible", "Small", "Dull",
+			"Unhappy", "Bulky", "Shameful", "Heavy", "Light", "Sighted", "Rapid", "Hasty", "Intimidating", "Deadly",
+			"Staunch", "Awful", "Lethargic", "Awkward", "Powerful", "Mystic", "Adept", "Masterful", "Inept", "Ignorant",
+			"Deranged", "Intense", "Taboo", "Celestial", "Furious", "Keen", "Superior", "Forceful", "Broken", "Damaged",
+			"Shoddy", "Quick", "Deadly", "Agile", "Nimble", "Murderous", "Slow", "Sluggish", "Lazy", "Annoying",
+			"Nasty", "Manic", "Hurtful", "Strong", "Unpleasant", "Weak", "Ruthless", "Frenzying", "Godly", "Demonic",
+			"Zealous", "Hard", "Guarding", "Armored", "Warding", "Arcane", "Precise", "Lucky", "Jagged", "Spiked",
+			"Angry", "Menacing", "Brisk", "Fleeting", "Hasty", "Quick", "Wild", "Rash", "Intrepid", "Violent",
+			"Legendary", "Unreal", "Mythical"
 		};
 	}
 }
