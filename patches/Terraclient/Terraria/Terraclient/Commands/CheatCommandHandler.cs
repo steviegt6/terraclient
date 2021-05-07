391f18d109e263a0d374a3a274c1b0294feacbc5
MarauderKnight3 <marauderknight3@gmail.com>
2021-05-06 21:17:42 -0700

    a lot of shit
    frick

--- patches/Terraclient/Terraria/Terraclient/Commands/CheatCommandHandler.cs
+++ patches/Terraclient/Terraria/Terraclient/Commands/CheatCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -25,12 +26,201 @@ namespace Terraria.Terraclient.Commands
 					break;
 				}
 			}
-			catch {
-				CheatCommandUtils.Output(true, "Something went wrong.");
+			catch (Exception e) {
+				CheatCommandUtils.Output(true, "Something went wrong. " + e.Message, 4);
 				CheatCommandUtils.Output(false, $"Registered text: {string.Join(" ", arguments)}");
 			}
 
 			return true;
+		}
+
+		public static List<object> Digest(List<string> arguments, List<CommandArgument> argumentDetails) {
+			if (argumentDetails.Count == 0) {
+				return CheatCommandUtils.ConvertToObjects(arguments);
+			}
+			List<object> polished = new List<object>();
+			//Quotation concatenation is handled before this preprocessing, effectively making this post-preprocessing.
+			for (int i = 0; i < argumentDetails.Count; i++) {
+				if (arguments.Count == 0 && !argumentDetails[i].MayBeSkipped) {
+					if (polished.Count == 0) {
+						CheatCommandUtils.Output(true, "This command requires arguments to run.", 1);
+					}
+					else {
+						List<string> missingArguments = new List<string>();
+						for (int j = i; j < argumentDetails.Count; j++) {
+							missingArguments.Add(argumentDetails[j].ArgumentName);
+						}
+						CheatCommandUtils.Output(true, "You did not submit all the required arguments."
+							+ " Every required argument will be before the optional arguments, no matter what command you run."
+							+ " Please try again. Missing arguments: " + string.Join(", ", missingArguments), 1);
+					}
+					goto Errored;
+				}
+				if (arguments.Count == 0)
+					break;
+
+				//declaring these here because declaring inside switch statements caused variable names to no longer be available
+				List<string> matches = new List<string> { };
+				string query = "";
+				int indexOfFirstOmitted = arguments.Count;
+
+				switch (argumentDetails[i].InputType) {
+					case CommandArgument.PositiveIntegerRange:
+						if (new Regex("\\D").IsMatch(arguments[0])) {
+							CheatCommandUtils.Output(true, argumentDetails[i].ArgumentName + " must be a positive integer.", 1);
+							goto Errored;
+						}
+
+					IAmANumber:
+
+						string parsingString = arguments[0];
+
+						while (parsingString.StartsWith("0"))
+							parsingString = parsingString.Remove(0, 1);
+
+						polished.Add(0);
+
+						if (parsingString.Length > argumentDetails[i].ExpectedInputs[1].ToString().Length)
+							polished[polished.Count - 1] = argumentDetails[i].ExpectedInputs[1];
+						else if (argumentDetails[i].ExpectedInputs[1].ToString().Length == 10 && Convert.ToInt64(parsingString) > 2147483647L)
+							polished[polished.Count - 1] = argumentDetails[i].ExpectedInputs[1];
+						else if (parsingString.Length > 0)
+							polished[polished.Count - 1] = int.Parse(parsingString);
+
+						arguments.RemoveAt(0);
+						break;
+
+					case CommandArgument.Text:
+						query = arguments[0].ToLower();
+
+						for (int j = 0; j < argumentDetails[i].ExpectedInputs.Count; j++)
+							if (CheatCommandUtils.ConvertToStrings(argumentDetails[i].ExpectedInputs)[j].ToLower().StartsWith(query))
+								matches.Add((string)argumentDetails[i].ExpectedInputs[j]);
+
+						switch (matches.Count) {
+							case 0:
+								CheatCommandUtils.Output(true, "Input for argument " + argumentDetails[i].ArgumentName + " did not autocomplete to or directly match any options for " + argumentDetails[i].ArgumentName + ".", 3);
+								goto Errored;
+
+							case > 1:
+								foreach (string thing in matches) {
+									if (thing.Equals(query)) {
+										polished.Add(thing);
+										matches.Remove(thing);
+										CheatCommandUtils.Output(false, "Input for argument " + argumentDetails[i].ArgumentName + " found a direct match (" + thing + "), skipping other results including " +
+											string.Join(", ", matches));
+										arguments.RemoveAt(0);
+										goto CanNowMoveToNextArgument;
+									}
+								}
+								CheatCommandUtils.Output(true, "Input for argument " + argumentDetails[i].ArgumentName + " was too unspecific; There was more than one selection. Make your query longer so it more closely matches the desired selection. Results included " +
+									string.Join(", ", matches), 2);
+								goto Errored;
+						}
+						polished.Add(matches[0]);
+						arguments.RemoveAt(0);
+						break;
+
+					case CommandArgument.TextConcatenationUntilNextInt:
+						for (int j = 1; j < arguments.Count; j++) {
+							if (new Regex("\\D").IsMatch(arguments[j]))
+								continue;
+							indexOfFirstOmitted = j;
+							break;
+						}
+						query = string.Join(" ", arguments.GetRange(0, indexOfFirstOmitted)).ToLower();
+
+						for (int j = 0; j < argumentDetails[i].ExpectedInputs.Count; j++)
+							if (CheatCommandUtils.ConvertToStrings(argumentDetails[i].ExpectedInputs)[j].ToLower().StartsWith(query))
+								matches.Add((string)argumentDetails[i].ExpectedInputs[j]);
+
+						switch (matches.Count) {
+							case 0:
+								CheatCommandUtils.Output(true, "Input for argument " + argumentDetails[i].ArgumentName + " did not autocomplete to or directly match any options for " + argumentDetails[i].ArgumentName + ".", 3);
+								goto Errored;
+
+							case > 1:
+								foreach (string thing in matches) {
+									if (thing.Equals(query)) {
+										polished.Add(thing);
+										matches.Remove(thing);
+										CheatCommandUtils.Output(false, "Input for argument " + argumentDetails[i].ArgumentName + " found a direct match (" + thing + "), skipping other results including " +
+											string.Join(", ", matches));
+										arguments.RemoveRange(0, indexOfFirstOmitted);
+										goto CanNowMoveToNextArgument;
+									}
+								}
+								CheatCommandUtils.Output(true, "Input for argument " + argumentDetails[i].ArgumentName + " was too unspecific; There was more than one selection. Make your query longer so it more closely matches the desired selection. Results included " +
+									string.Join(", ", matches), 2);
+								goto Errored;
+						}
+						polished.Add(matches[0]);
+						arguments.RemoveRange(0, indexOfFirstOmitted);
+						break;
+
+					case CommandArgument.PositiveIntegerRangeOrTextConcatenationUntilNextInt:
+						if (!new Regex("\\D").IsMatch(arguments[0])) {
+							goto IAmANumber;
+						}
+						for (int j = 1; j < arguments.Count; j++) {
+							if (new Regex("\\D").IsMatch(arguments[j]))
+								continue;
+							indexOfFirstOmitted = j;
+							break;
+						}
+						query = string.Join(" ", arguments.GetRange(0, indexOfFirstOmitted)).ToLower();
+
+						for (int j = 0; j < argumentDetails[i].ExpectedInputs.Count; j++)
+							if (CheatCommandUtils.ConvertToStrings(argumentDetails[i].ExpectedInputs)[j].ToLower().StartsWith(query))
+								matches.Add((string)argumentDetails[i].ExpectedInputs[j]);
+
+						switch (matches.Count) {
+							case 0:
+								CheatCommandUtils.Output(true, "Input for argument " + argumentDetails[i].ArgumentName + " did not autocomplete to or directly match any options for " + argumentDetails[i].ArgumentName + ".", 3);
+								goto Errored;
+
+							case > 1:
+								foreach (string thing in matches) {
+									if (thing.Equals(query)) {
+										polished.Add(thing);
+										matches.Remove(thing);
+										CheatCommandUtils.Output(false, "Input for argument " + argumentDetails[i].ArgumentName + " found a direct match (" + thing + "), skipping other results including " +
+											string.Join(", ", matches));
+										arguments.RemoveRange(0, indexOfFirstOmitted);
+										goto CanNowMoveToNextArgument;
+									}
+								}
+								CheatCommandUtils.Output(true, "Input for argument " + argumentDetails[i].ArgumentName + " was too unspecific; There was more than one selection. Make your query longer so it more closely matches the desired selection. Results included " +
+									string.Join(", ", matches), 2);
+								goto Errored;
+						}
+						polished.Add(matches[0]);
+						arguments.RemoveRange(0, indexOfFirstOmitted);
+						break;
+
+					case CommandArgument.CustomText:
+						polished.Add(arguments[0]);
+						arguments.RemoveAt(0);
+						break;
+
+					case CommandArgument.CustomTextConcatenationUntilNextInt:
+						for (int j = 1; j < arguments.Count; j++) {
+							if (new Regex("\\D").IsMatch(arguments[j]))
+								continue;
+							indexOfFirstOmitted = j;
+							break;
+						}
+						polished.Add(string.Join(" ", arguments.GetRange(0, indexOfFirstOmitted)));
+						arguments.RemoveRange(0, indexOfFirstOmitted);
+						break;
+				}
+
+			CanNowMoveToNextArgument:
+				continue;
+			}
+			return polished;
+		Errored:
+			return new List<object> { false };
 		}
 
 		internal static void UpdateColors() {
---
+++ patches/Terraclient/Terraria/Terraclient/Commands/CommandArgument.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+
+namespace Terraria.Terraclient.Commands
+{
+	public class CommandArgument
+	{
+		public string ArgumentName;
+
+		public List<object> ExpectedInputs;
+
+		public byte InputType;
+
+		public bool MayBeSkipped;
+
+		public const byte PositiveIntegerRange = 0;
+
+		public const byte Text = 1;
+
+		public const byte TextConcatenationUntilNextInt = 2;
+
+		public const byte PositiveIntegerRangeOrTextConcatenationUntilNextInt = 3;
+
+		public const byte CustomText = 4;
+
+		public const byte CustomTextConcatenationUntilNextInt = 5;
+
+		public CommandArgument(string name, List<object> inputs, bool concat, bool skippable = false) {
+			//Assumptions:
+			/*
+			First, inputs will be any of these things:
+			If Concat is true,
+				it is possible that it is {}: Custom concat reaching the next integer or the end of the argument list.
+				it is possible that it is {int, int, string...}: Either a positive integer range (the first two ints) or any one of the inputs after that.
+				it is possible that it is {string...}: Concat that may match any of these values (or autocomplete to them).
+			or,	then
+				it is possible that it is {int, int}: A range of expected integers, clamped to this minimum and maximum.
+				it is possible that it is {}: Any single word of text.
+				it is possible that it is {string...}: A single word of text that may match any of these values (or autocomplete to them).
+
+			the Name parameter is the display name that will be shown to the player when necessary. It should be capitalized.
+
+			the first CommandArgument that is set to Skippable in a list of CommandArguments will render all
+			other CommandArguments further along the list also "skippable", regardless of their setting, because math.
+
+			If any command would be better off handling the more raw userinputted arguments by itself, the list of CommandArguments should be inputted blank when creating the command.
+
+			Please keep in mind that when comparing strings, CheatCommandHandler.Digest() will always make all strings lowercase. This can be changed if needed, with a later patch.
+			Custom text input types won't be turned lowercase before the effective argument is polished and returned by CheatCommandHandler.Digest().
+
+			All commands that use this system should work on the assumption that the incoming arguments have been pre-processed: If syntax is wrong when a player sends in a command,
+			it will be caught there. If the command does end up getting run, it should expect that all variables are ready to use.
+			 */
+			ArgumentName = name;
+			ExpectedInputs = inputs;
+			if (concat) {
+				if (inputs.Count == 0)
+					InputType = CustomTextConcatenationUntilNextInt;
+				else if (inputs[0] is int && inputs[1] is int)
+					InputType = PositiveIntegerRangeOrTextConcatenationUntilNextInt;
+				else
+					InputType = TextConcatenationUntilNextInt;
+			}
+			else {
+				if (inputs.Count == 2 && inputs[0] is int && inputs[1] is int)
+					InputType = PositiveIntegerRange;
+				else if (inputs.Count == 0)
+					InputType = CustomText;
+				else
+					InputType = Text;
+			}
+			MayBeSkipped = skippable;
+		}
+	}
+}
