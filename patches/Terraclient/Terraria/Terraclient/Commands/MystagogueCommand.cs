ac054d4329f80daa191a9e926c9e2308e0e9f1e3
MarauderKnight3 <marauderknight3@gmail.com>
2021-05-11 13:28:08 -0700

    edit help, torch, and add stack hacks

--- patches/Terraclient/Terraria/Terraclient/Commands/MystagogueCommand.cs
+++ patches/Terraclient/Terraria/Terraclient/Commands/MystagogueCommand.cs
@@ -95,7 +95,7 @@ namespace Terraria.Terraclient.Commands
 
 						CheatCommandUtils.Output(false,
 							"You chose to read about: \"" + match.CommandName + "\".\nDescription: " +
-							match.CommandDescription + "\nArguments:" + argsText);
+							match.CommandDescription + "\nArguments:" + (argsText.Length > 0 ? argsText : "There are none."));
 					}
 					else {
 						CheatCommandUtils.Output(false,
@@ -227,15 +227,149 @@ namespace Terraria.Terraclient.Commands
 				})
 				.Build();
 
+			Create("setstacks2b", "Sets the stack of all stackable items in the inventory and all the banks to 2147483647.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					foreach (Item item in Main.player[Main.myPlayer].inventory) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].miscEquips) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank2.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank3.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank4.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = int.MaxValue;
+					}
+					CheatCommandUtils.Output(false, "Stacks overflown.");
+				})
+				.Build();
+
+			Create("setstackslegit", "Limits the stack of all stackable items in the inventory and all the banks to their maxstacks.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					foreach (Item item in Main.player[Main.myPlayer].inventory) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].armor) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].dye) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].miscEquips) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].miscDyes) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank2.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank3.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank4.item) {
+						if (item.IsAir || ContentSamples.ItemsByType[item.type].maxStack == 1)
+							continue;
+						item.stack = item.stack > ContentSamples.ItemsByType[item.type].maxStack ? ContentSamples.ItemsByType[item.type].maxStack : item.stack;
+					}
+					CheatCommandUtils.Output(false, "Stacks regulated.");
+				})
+				.Build();
+
+			Create("setmaxstacks2b", "Sets the max stack of all stackable items in the inventory and void to their default values.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					foreach (Item item in Main.player[Main.myPlayer].inventory) {
+						if (item.IsAir)
+							continue;
+						item.maxStack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].miscEquips) {
+						if (item.IsAir)
+							continue;
+						item.maxStack = int.MaxValue;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank4.item) {
+						if (item.IsAir)
+							continue;
+						item.maxStack = int.MaxValue;
+					}
+					CheatCommandUtils.Output(false, "Max stacks expanded.");
+				})
+				.Build();
+
+			Create("setmaxstackslegit", "Sets the max stack of all stackable items in the inventory and void to 2147483647.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					foreach (Item item in Main.player[Main.myPlayer].inventory) {
+						if (item.IsAir)
+							continue;
+						item.maxStack = ContentSamples.ItemsByType[item.type].maxStack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].miscEquips) {
+						if (item.IsAir)
+							continue;
+						item.maxStack = ContentSamples.ItemsByType[item.type].maxStack;
+					}
+					foreach (Item item in Main.player[Main.myPlayer].bank4.item) {
+						if (item.IsAir)
+							continue;
+						item.maxStack = ContentSamples.ItemsByType[item.type].maxStack;
+					}
+					CheatCommandUtils.Output(false, "Max stacks regulated.");
+				})
+				.Build();
+
 			Create("torch",
 					"Gives the player a torch with an invalid torch ID, which can cause crashes on other clients and the server.")
 				.AddParameters(new List<CommandArgument>())
 				.AddAction(_ => {
+					HandyFunctions.MoveMouseItemToInventory();
 					Main.mouseItem.SetDefaults(ItemID.Torch);
 					Main.mouseItem.stack = 1;
 					Main.mouseItem.Refresh();
 					Main.mouseItem.placeStyle = 99;
-					Main.mouseItem.SetNameOverride("really annoying (and totally safe) place-able object");
+					Main.mouseItem.SetNameOverride("torch of loneliness");
 				})
 				.Build();
 
