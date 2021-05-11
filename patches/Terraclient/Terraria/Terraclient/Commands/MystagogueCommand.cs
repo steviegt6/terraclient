d4b5de091e21bcee51c2c38e3cb3ef2b43e7eed8
MarauderKnight3 <marauderknight3@gmail.com>
2021-05-10 23:05:12 -0700

    move items command back where it goes

--- patches/Terraclient/Terraria/Terraclient/Commands/MystagogueCommand.cs
+++ patches/Terraclient/Terraria/Terraclient/Commands/MystagogueCommand.cs
@@ -34,8 +34,8 @@ namespace Terraria.Terraclient.Commands
 
 		// TODO: localization
 		static MystagogueCommand() {
-			List<object> idsRangeItemNames = new List<object> {0, ItemID.Count - 1};
-			List<object> idsRangePrefixNames = new List<object> {0, Prefixes.Length - 1};
+			List<object> idsRangeItemNames = new List<object> { 0, ItemID.Count - 1 };
+			List<object> idsRangePrefixNames = new List<object> { 0, Prefixes.Length - 1 };
 			idsRangeItemNames.AddRange(CheatCommandUtils.ItemNames.Values);
 			idsRangePrefixNames.AddRange(Prefixes);
 
@@ -55,11 +55,11 @@ namespace Terraria.Terraclient.Commands
 
 						foreach (CommandArgument arg in match.CommandArgumentDetails[0]) {
 							argsText += (argsText.Length > 0 ? ", " : "") + "(" + arg.ArgumentName +
-							            ": Input type: Input accepts ";
+										": Input type: Input accepts ";
 							switch (arg.InputType) {
 								case CommandArgument.ArgInputType.PositiveIntegerRange:
 									argsText += "a number in between " + arg.ExpectedInputs[0] + " and " +
-									            arg.ExpectedInputs[1];
+												arg.ExpectedInputs[1];
 									break;
 
 								case CommandArgument.ArgInputType.Text:
@@ -68,7 +68,7 @@ namespace Terraria.Terraclient.Commands
 
 								case CommandArgument.ArgInputType.PositiveIntegerRangeOrText:
 									argsText += "specific text options or a number in between " +
-									            arg.ExpectedInputs[0] + " and " + arg.ExpectedInputs[1];
+												arg.ExpectedInputs[0] + " and " + arg.ExpectedInputs[1];
 									break;
 
 								case CommandArgument.ArgInputType.TextConcatenationUntilNextInt:
@@ -78,7 +78,7 @@ namespace Terraria.Terraclient.Commands
 								case CommandArgument.ArgInputType
 									.PositiveIntegerRangeOrTextConcatenationUntilNextInt:
 									argsText += "specific text options or a number in between " +
-									            arg.ExpectedInputs[0] + " and " + arg.ExpectedInputs[1];
+												arg.ExpectedInputs[0] + " and " + arg.ExpectedInputs[1];
 									break;
 
 								case CommandArgument.ArgInputType.CustomText:
@@ -106,99 +106,6 @@ namespace Terraria.Terraclient.Commands
 					}
 				})
 				.Build();
-
-			Create("searchitems",
-					"Search all the items by name using a keyword. Can be used to get the item ID of an item from its name.")
-				.AddParameters(new List<CommandArgument> {new("Keyword to search for", new List<object>(), true)})
-				.AddAction(args => {
-					List<string> matches = new List<string>();
-					for (int j = 1; j < CheatCommandUtils.ItemNames.Count; j++)
-						if (CheatCommandUtils.ItemNames[j]
-							.Contains((string)args[0], StringComparison.OrdinalIgnoreCase))
-							matches.Add(CheatCommandUtils.ItemNames[j] + " (" + CheatCommandUtils.ItemNames.Values
-								.ToList().IndexOf(CheatCommandUtils.ItemNames[j]) + ")");
-					if (matches.Count < 1) {
-						CheatCommandUtils.Output(false, "No item names match.");
-						return;
-					}
-
-					if (matches.Count > 1)
-						matches.Sort();
-					CheatCommandUtils.Output(false, "Found these items: " + string.Join(", ", matches));
-				})
-				.Build();
-
-			Create("ri", "Refreshes the item selected in the hotbar.")
-				.AddParameters(new List<CommandArgument>())
-				.AddAction(_ => {
-					Main.player[Main.myPlayer].HeldItem.Refresh();
-					HandyFunctions.ToolGodBuffMyTools();
-					CheatCommandUtils.Output(false, "Selected item refreshed.");
-				})
-				.Build();
-
-			Create("ris", "Refreshes all items in all inventories of the character.")
-				.AddParameters(new List<CommandArgument>())
-				.AddAction(_ => {
-					Main.player[Main.myPlayer].RefreshItems();
-					HandyFunctions.ToolGodBuffMyTools();
-					CheatCommandUtils.Output(false, "All inventory items refreshed.");
-				})
-				.Build();
-
-			Create("invclear",
-					"Turns all items that are not favorited in the inventory and all items in the Void inventory into air.")
-				.AddParameters(new List<CommandArgument>())
-				.AddAction(_ => {
-					foreach (Item item in Main.player[Main.myPlayer].inventory) {
-						if (!item.favorited) {
-							item.SetDefaults();
-						}
-					}
-
-					foreach (Item item in Main.player[Main.myPlayer].bank4.item) {
-						item.SetDefaults();
-					}
-          
-					HandyFunctions.ToolGodBuffMyTools();
-          
-					CheatCommandUtils.Output(false,
-						"Cleaned all items not favorited in the inventory and all items in the Void inventory.");
-				})
-				.Build();
-
-			Create("toolgod", "Toggles the Tool God Cheat in the Cheat Menu.")
-				.AddParameters(new List<CommandArgument>())
-				.AddAction(_ => {
-					CheatHandler.GetCheat<ToolGodCheat>().Toggle();
-					HandyFunctions.ToolGodBuffMyTools();
-					CheatCommandUtils.Output(false, "Tool god toggled " + (CheatHandler.GetCheat<ToolGodCheat>().isEnabled ? "on" : "off") + ".");
-				})
-				.Build();
-
-			Create("torch",
-					"Gives the player a torch with an invalid torch ID, which can cause crashes on other clients and the server.")
-				.AddParameters(new List<CommandArgument>())
-				.AddAction(_ => {
-					Main.mouseItem.SetDefaults(ItemID.Torch);
-					Main.mouseItem.stack = 1;
-					Main.mouseItem.Refresh();
-					Main.mouseItem.placeStyle = 99;
-					Main.mouseItem.SetNameOverride("really annoying (and totally safe) place-able object");
-				})
-				.Build();
-
-			/*Create("unbreakable",
-					"Gives the player a stack of unbreakable dirt blocks that, when placed, are not rendered on any client.")
-				.AddParameters(new List<CommandArgument>())
-				.AddAction(_ => {
-					Main.mouseItem.SetDefaults(ItemID.DirtBlock);
-					Main.mouseItem.stack = 999;
-					Main.mouseItem.Refresh();
-					Main.mouseItem.placeStyle = 99;
-					Main.mouseItem.SetNameOverride("dirt but it's really stupid");
-				})
-				.Build();*/
 
 			Create("i",
 					"Spawns an item by converting your currently-held cursor item (or thin air) to it. " +
@@ -251,6 +158,99 @@ namespace Terraria.Terraclient.Commands
 						$"Set cursor item to {Main.mouseItem.stack}{(Main.mouseItem.prefix > 0 ? (" " + Prefixes[Main.mouseItem.prefix]) : "")} {Lang.GetItemNameValue(Main.mouseItem.type)} ({Main.mouseItem.type})");
 				})
 				.Build();
+
+			Create("searchitems",
+					"Search all the items by name using a keyword. Can be used to get the item ID of an item from its name.")
+				.AddParameters(new List<CommandArgument> { new("Keyword to search for", new List<object>(), true) })
+				.AddAction(args => {
+					List<string> matches = new List<string>();
+					for (int j = 1; j < CheatCommandUtils.ItemNames.Count; j++)
+						if (CheatCommandUtils.ItemNames[j]
+							.Contains((string)args[0], StringComparison.OrdinalIgnoreCase))
+							matches.Add(CheatCommandUtils.ItemNames[j] + " (" + CheatCommandUtils.ItemNames.Values
+								.ToList().IndexOf(CheatCommandUtils.ItemNames[j]) + ")");
+					if (matches.Count < 1) {
+						CheatCommandUtils.Output(false, "No item names match.");
+						return;
+					}
+
+					if (matches.Count > 1)
+						matches.Sort();
+					CheatCommandUtils.Output(false, "Found these items: " + string.Join(", ", matches));
+				})
+				.Build();
+
+			Create("ri", "Refreshes the item selected in the hotbar.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					Main.player[Main.myPlayer].HeldItem.Refresh();
+					HandyFunctions.ToolGodBuffMyTools();
+					CheatCommandUtils.Output(false, "Selected item refreshed.");
+				})
+				.Build();
+
+			Create("ris", "Refreshes all items in all inventories of the character.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					Main.player[Main.myPlayer].RefreshItems();
+					HandyFunctions.ToolGodBuffMyTools();
+					CheatCommandUtils.Output(false, "All inventory items refreshed.");
+				})
+				.Build();
+
+			Create("invclear",
+					"Turns all items that are not favorited in the inventory and all items in the Void inventory into air.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					foreach (Item item in Main.player[Main.myPlayer].inventory) {
+						if (!item.favorited) {
+							item.SetDefaults();
+						}
+					}
+
+					foreach (Item item in Main.player[Main.myPlayer].bank4.item) {
+						item.SetDefaults();
+					}
+
+					HandyFunctions.ToolGodBuffMyTools();
+
+					CheatCommandUtils.Output(false,
+						"Cleaned all items not favorited in the inventory and all items in the Void inventory.");
+				})
+				.Build();
+
+			Create("toolgod", "Toggles the Tool God Cheat in the Cheat Menu.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					CheatHandler.GetCheat<ToolGodCheat>().Toggle();
+					HandyFunctions.ToolGodBuffMyTools();
+					CheatCommandUtils.Output(false, "Tool god toggled " + (CheatHandler.GetCheat<ToolGodCheat>().isEnabled ? "on" : "off") + ".");
+				})
+				.Build();
+
+			Create("torch",
+					"Gives the player a torch with an invalid torch ID, which can cause crashes on other clients and the server.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					Main.mouseItem.SetDefaults(ItemID.Torch);
+					Main.mouseItem.stack = 1;
+					Main.mouseItem.Refresh();
+					Main.mouseItem.placeStyle = 99;
+					Main.mouseItem.SetNameOverride("really annoying (and totally safe) place-able object");
+				})
+				.Build();
+
+			/*Create("unbreakable",
+					"Gives the player a stack of unbreakable dirt blocks that, when placed, are not rendered on any client.")
+				.AddParameters(new List<CommandArgument>())
+				.AddAction(_ => {
+					Main.mouseItem.SetDefaults(ItemID.DirtBlock);
+					Main.mouseItem.stack = 999;
+					Main.mouseItem.Refresh();
+					Main.mouseItem.placeStyle = 99;
+					Main.mouseItem.SetNameOverride("dirt but it's really stupid");
+				})
+				.Build();*/
 
 			CommandList[0].CommandArgumentDetails[0][0].ExpectedInputs = CommandListNames.ToList<object>();
 		}
