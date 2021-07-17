96357fbc8df150e18c0c12a82879959b47fb7c4f
Stevie <xxlennygamerxx@gmail.com>
2021-07-16 18:04:29 -0700

    experimental gtk

---
+++ patches/Terraclient/Terraria/Utilities/FileBrowser/GtkWindow.cs
@@ -0,0 +1,21 @@
+﻿using Gtk;
+
+namespace Terraria.Utilities.FileBrowser
+{
+	public class GtkWindow : Window
+	{
+		private GtkWindow() : base(WindowType.Toplevel) {
+			Title = "File selection window";
+			SetDefaultSize(350, 250);
+			DeleteEvent += (_, _) => Application.Quit();
+			Show();
+		}
+
+		internal static GtkWindow GetWindow() {
+			Application.Init();
+			GtkWindow window = new GtkWindow();
+			Application.Run();
+			return window;
+		}
+	}
+}
